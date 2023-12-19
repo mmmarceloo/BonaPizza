b7979800069802f5265e4fc152e06f3522838082
Marcelo Moreira <mmmarceloo@yahoo.com.br>
2023-12-19 11:26:59 -0300

    redirecionei o site para o anotaAi e coloquei um contador no db

---
+++ BonaPizza/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using BonaPizza.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BonaPizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Visualizacao.Salvar();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BonaPizza/Models/Visualizacao.cs
@@ -0,0 +1,31 @@
+﻿using MySqlConnector;
+namespace BonaPizza.Models
+{
+    public class Visualizacao
+    {
+        
+        
+        private readonly static string _conn = "server=*;user=*;password=*;database=*;";
+
+        public static void Salvar()
+        {
+            var sql = "INSERT INTO VISITAS(DATA_ENTRADA) VALUES(NOW())";
+            try
+            {
+                using (var cn = new MySqlConnection(_conn))
+                {
+                    cn.Open();
+                    using (var cmd = new MySqlCommand(sql, cn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Falha!" + ex.Message);
+            }
+        }
+    }
+}
